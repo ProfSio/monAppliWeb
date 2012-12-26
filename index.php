54b8f3eab362cbf951ef9df061e8c14998e3a5bf
dev <dev@lyc-lecastel.fr>
2012-12-26 18:26:19 +0100

    Maj bis
    Signed-off-by: dev <dev@lyc-lecastel.fr>

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
  if (!isset($GET['page']))
 	include 'calculatrice.html'; 
 	else {
-   if($GET['page']=="articles")
-	 include 'articles.html';
-	 if($GET['page']=="nous-contacter")
-	 include 'nous-contacter.html';}
+		if($GET['page']=="articles")
+			include 'articles.html';
+		if($GET['page']=="nous-contacter")
+			include 'nous-contacter.html';}
  include 'footer.html'; ?>
 
